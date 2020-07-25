6abaf70823a80d597acc035f58fa125d15ab71f5
qureshi.nomaan <nqafzal@gmail.com>
2020-07-25 16:01:33 +0530

    PID control, needs improvment

--- Control/1dControl.py
+++ Control/1dControl.py
@@ -6,7 +6,7 @@
 # Defining the state parameters #
 #======================================================================================#
 x_target = 0.0
-x_initial = 100.0
+x_initial = -100.0
 v_initial = 0.0
 v_max = 5
 next = lambda x, v : x+v
@@ -25,11 +25,11 @@ fig = plt.figure()
 axis = plt.axes(xlim =(-100, 100),
                 ylim =(-100, 100))
   
-line = axis.scatter([x_initial, x_target], [0, 0], s = 50, c=['g','r'], marker='o')
+line = axis.scatter( [0, 0], [x_initial, x_target], s = 50, c=['g','r'], marker='o')
   
 
 def init():
-    line.set_offsets([[x_initial, 0], [x_target, 0]])
+    line.set_offsets([[0, x_initial], [0, x_target]])
     return line,
   
 #======================================================================================#
@@ -39,7 +39,7 @@ def init():
 #======================================================================================#
 # Defining the control Parameters #
 #======================================================================================#
-K_p, K_d, K_i = 1, 1, 1
+K_p, K_d, K_i = 0.5, 0.2, 0.01
 #======================================================================================#
 
 #======================================================================================#
@@ -49,27 +49,25 @@ K_p, K_d, K_i = 1, 1, 1
 
 x_current = x_initial
 v_current = v_initial
-
+v_last, integral = 0, 0
 def K_Control(i):
-    global x_current, v_current
+    global x_current, v_current, v_last, integral
     sensor_reading = x_current + random.gauss(0, 0.5)
     error = x_target - sensor_reading
-    if error < 0.3 and error > 0.3 :
-        v_current = 0
-    elif error > 0 :
-        v_current = min(v_max, K_p * error) + random.gauss(0, min(v_max, K_p * error)/10)
-    else :
-        v_current = max(-v_max, K_p * error) + random.gauss(0, max(-v_max, K_p * error)/10)
-    x_current = x_current + v_current
+    v_current = K_p * error - K_d * v_last + K_i * integral
+    v_last = v_current
+    integral += error
+    
+    x_current = x_current + v_current - 3 + random.gauss(0, v_current/100)
     print(error, x_current, v_current)
-    line.set_offsets([[x_current, 0], [x_target, 0]])
+    line.set_offsets([[0, x_current], [0, x_target]])
     
     return line,
     
 anim = animation.FuncAnimation(fig, K_Control,
                             init_func = init,
                             frames = 500000,
-                            interval = 100)
+                            interval = 1000)
 plt.show()
     
 
